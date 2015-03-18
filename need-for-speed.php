59d239a46eb07bbb91ddf2aa6f5e1a842afd1640
Eduard Urbach <e.urbach@gmail.com>
2015-03-18 10:42:57 +0900

    Fixed whitespace problem

--- need-for-speed.php
+++ need-for-speed.php
@@ -1,28 +1,28 @@
 <?php
-    // Load config
-    $config = json_decode(file_get_contents('config.json'), true);
-    
+	// Load config
+	$config = json_decode(file_get_contents('config.json'), true);
+	
 	// Setup
-    $iterations = $config['iterations'];
-    $dbConfig = $config['database'];
-    $namespace = $dbConfig['namespace'];
-    $table = $dbConfig['table']
-    $record = null;
+	$iterations = $config['iterations'];
+	$dbConfig = $config['database'];
+	$namespace = $dbConfig['namespace'];
+	$table = $dbConfig['table']
+	$record = null;
 
 	$db = new Aerospike($config['aerospike']);
-    
-    // Start
+	
+	// Start
 	$before = microtime(true);
 
 	for($i = 0; $i < $iterations; $i++) {
-        $keyName = "test_$i";
+		$keyName = "test_$i";
 		$key = $db->initKey($namespace, $table, $keyName);
 		$db->get($key, $record);
 		$data = $record['bins'];
 	}
-    
-    // End
+	
+	// End
 	$after = microtime(true);
-    
+	
 	echo 'Aerospike: ' . ($after - $before) / $i . ' sec/GET<br>';
 ?>
